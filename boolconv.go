1e9c61262bb9345ac6fcb1acd7b01606598a6b7b
osamingo <osamingo@gmail.com>
2015-07-16 01:01:51 +0900

    Initial commit

---
+++ boolconv.go
@@ -0,0 +1,80 @@
+package boolconv
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+)
+
+// Bool is a wrap of byte.
+type Bool byte
+
+const (
+	// False means false on Bool.
+	False Bool = iota
+	// True means true on Bool.
+	True
+)
+
+// NewBoolByBytes converts bytes into Bool.
+func NewBoolByBytes(b []byte) Bool {
+	return Bool(b[0])
+}
+
+// NewBoolBybool converts bool into Bool.
+func NewBoolBybool(b bool) Bool {
+	if b {
+		return True
+	}
+	return False
+}
+
+// NewBoolByInterface converts interface into Bool.
+func NewBoolByInterface(i interface{}) (Bool, error) {
+
+	t := reflect.TypeOf(i)
+	val := reflect.ValueOf(i)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	if t.Name() == "Bool" {
+		return (val.Interface()).(Bool), nil
+	}
+
+	switch val.Kind() {
+	case reflect.Bool:
+		return NewBoolBybool(i.(bool)), nil
+	case reflect.Uint8:
+		return Bool(i.(byte)), nil
+	case reflect.String:
+		if b, err := strconv.ParseBool(i.(string)); err == nil {
+			return NewBoolBybool(b), nil
+		}
+	case reflect.Slice:
+		v := val.Index(0)
+		if b, ok := (v.Interface()).(byte); ok {
+			return Bool(b), nil
+		}
+	}
+
+	return False, errors.New("unsupported type")
+}
+
+// Tob returns bool.
+func (b Bool) Tob() bool {
+	return b == True
+}
+
+// Bytes returns []byte.
+func (b Bool) Bytes() []byte {
+	return []byte{byte(b)}
+}
+
+// String returns string.
+func (b Bool) String() string {
+	if b == True {
+		return "true"
+	}
+	return "false"
+}
---
+++ boolconv_test.go
@@ -0,0 +1,127 @@
+package boolconv
+
+import "testing"
+
+func TestNewBoolByBytes(t *testing.T) {
+
+	ret := NewBoolByBytes([]byte{0})
+	if ret == True {
+		t.Error("expected boolconv.Flase got boolconv.True")
+	}
+
+	ret = NewBoolByBytes([]byte{1})
+	if ret == False {
+		t.Error("expected boolconv.True got boolconv.False")
+	}
+
+}
+
+func TestNewBoolBybool(t *testing.T) {
+
+	ret := NewBoolBybool(false)
+	if ret == True {
+		t.Error("expected boolconv.Flase got boolconv.True")
+	}
+
+	ret = NewBoolBybool(true)
+	if ret == False {
+		t.Error("expected boolconv.True got boolconv.False")
+	}
+
+}
+
+func TestNewBoolByInterface(t *testing.T) {
+
+	_, err := NewBoolByInterface(true)
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(false)
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(True)
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(False)
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(True.Bytes())
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(False.Bytes())
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(True.String())
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(False.String())
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(byte(1))
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	_, err = NewBoolByInterface(byte(0))
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	// should err
+	_, err = NewBoolByInterface([]string{"test"})
+	if err == nil {
+		t.Error("error is occurred")
+	}
+
+}
+
+func TestTob(t *testing.T) {
+
+	if False.Tob() {
+		t.Error("expected false got true")
+	}
+
+	if !True.Tob() {
+		t.Error("expected true got false")
+	}
+
+}
+
+func TestBytes(t *testing.T) {
+
+	if False.Bytes()[0] == byte(True) {
+		t.Error("expected boolconv.Flase got boolconv.True")
+	}
+
+	if True.Bytes()[0] == byte(False) {
+		t.Error("expected boolconv.True got boolconv.False")
+	}
+
+}
+
+func TestString(t *testing.T) {
+
+	if False.String() != "false" {
+		t.Error("expected \"false\" got ", False.String())
+	}
+
+	if True.String() != "true" {
+		t.Error("expected \"true\" got ", True.String())
+	}
+
+}
